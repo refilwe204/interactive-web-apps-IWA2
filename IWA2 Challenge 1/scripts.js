0fd2abd47f7d3849498e4aa5a9c25577323fc7de
refilwe204 <nqatyelwaangelin@gmail.com>
2023-03-28 19:32:37 +0200

    #commit to a new folder

---
+++ IWA2 Challenge 1/scripts.js
@@ -0,0 +1,9 @@
+// This is the firstName of the user
+
+const user = 'John'
+
+
+// This is the lastName of the user /* const surname = 'Smith'
+
+
+console.log(user, surname);
---
+++ IWA2 Challenge 2/scripts.js
@@ -0,0 +1,8 @@
+/** (c) ACME Inc. 2010 */
+
+/*
+ * This is the date that a user created their account 
+ */
+const date = '07/10/2014'
+
+console.log(date)
